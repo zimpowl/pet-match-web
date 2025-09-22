66bb87568e077a0794dd6f0f6d7c8cab822b41d3
Paul Ziwiakowsky <ziwiakowsky.paul@gmail.com>
2025-09-22 14:21:44 +0200

    [PET-MATCH-WEB] - add google tag script

---
+++ public/js/gtag.js
@@ -0,0 +1,14 @@
+// Chargement du script gtag.js
+(function() {
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = "https://www.googletagmanager.com/gtag/js?id=AW-10870475322";
+  document.head.appendChild(script);
+})();
+
+// Configuration Google Ads
+window.dataLayer = window.dataLayer || [];
+function gtag(){ dataLayer.push(arguments); }
+
+gtag('js', new Date());
+gtag('config', 'AW-10870475322');
